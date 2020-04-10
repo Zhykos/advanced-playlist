0745a6b3beb132f943596b0f9fc6c81ade8774d6
Thomas Cicognani <zhykos@gmail.com>
2020-04-10 16:44:54 +0200

    Push data in database

--- youtube-custom-feed/public/javascripts/ycf.js
+++ youtube-custom-feed/public/javascripts/ycf.js
@@ -4,7 +4,8 @@ function authenticate() {
     .then(
       function () {
         console.log("Sign-in successful");
-      }, function (err) {
+      },
+      function (err) {
         console.error("Error signing in", err);
       });
 }
@@ -15,7 +16,8 @@ function loadClient() {
     .then(
       function () {
         console.log("GAPI client loaded for API");
-      }, function (err) {
+      },
+      function (err) {
         console.error("Error loading GAPI client for API", err);
       });
 }
@@ -40,7 +42,8 @@ function insertDataFromChannel(channelId) {
       } else {
         insertDataFromUploadedPlaylist(uploadItems[0].contentDetails.relatedPlaylists.uploads)
       }
-    }, function (err) {
+    },
+    function (err) {
       console.error("Execute error", err);
     });
 }
@@ -58,7 +61,8 @@ function insertDataFromUploadedPlaylist(uploadPlaylistId) {
       } else {
         videoItems.forEach(video => insertDataFromVideoId(video.contentDetails.videoId));
       }
-    }, function (err) {
+    },
+    function (err) {
       console.error("Execute error", err);
     });
 }
@@ -69,10 +73,28 @@ function insertDataFromVideoId(videoId) {
     "id": videoId
   }).then(
     function (response) {
-      console.log("Response", response);
-      const html = generateHTMLFrom(response);
-      $("#videos").append(html);
-    }, function (err) {
+      //console.log("Response", response);
+      //const html = generateHTMLFrom(response);
+      //$("#videos").append(html);
+      const videoItems = response.result.items;
+      if (videoItems.length == 0) {
+        console.error(`No video found with ID '${videoId}'.`);
+      } else {
+        const video = videoItems[0];
+        const videoData = {
+          videoId: videoId,
+          videoThumbnailSrc: video.snippet.thumbnails.default.url,
+          videoTitle: video.snippet.title,
+          channelTitle: video.snippet.channelTitle,
+          videoPlayer: video.player.embedHtml
+        };
+        $.post("/addVideoInDatabase", videoData,
+          function (data) {
+            console.log("ok");
+          });
+      }
+    },
+    function (err) {
       console.error("Execute error", err);
     });
 }
