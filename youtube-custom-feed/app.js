e48df0ab9634ae85c9eefccab6d8f882957f1318
Thomas Cicognani <zhykos@gmail.com>
2020-04-29 13:26:09 +0200

    Clean main page

--- youtube-custom-feed/app.js
+++ youtube-custom-feed/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',
   function (req, res) {
-    res.render('index', { title: 'Express', videos: db.get('videos').value() });
+    res.render('index', { videos: db.get('videos').value() });
   }
 );
 
