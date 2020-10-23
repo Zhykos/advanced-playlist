3ea9b162d1bb570bfda9f97c593a7dbcb3356b4b
Thomas Cicognani <zhykos@gmail.com>
2020-10-23 17:08:56 +0200

    Detect parameters JSON template still used

--- youtube-custom-feed/public/javascripts/ycf-client.js
+++ youtube-custom-feed/public/javascripts/ycf-client.js
@@ -2,24 +2,32 @@ var ycf;
 
 readTextFile(function (ycfJson) {
   ycf = JSON.parse(ycfJson);
-
-  gapi.load("client:auth2",
-    function () {
-      gapi.auth2.init({ client_id: ycf.clientId }).then(
+  if (ycf) {
+    if (ycf.clientApiKey == "<XXX>" || ycf.clientId == "<YYY>.apps.googleusercontent.com") {
+      $("#loading").hide();
+      $("#contents").show();
+      $("#settings-error").show();
+      $("#connection").hide();
+    } else {
+      gapi.load("client:auth2",
         function () {
-          const GoogleAuth = gapi.auth2.getAuthInstance();
-          if (GoogleAuth.isSignedIn.get()) {
-            loadClient(function () {
-              $("#loading").hide();
-              $("#contents").show();
+          gapi.auth2.init({ client_id: ycf.clientId }).then(
+            function () {
+              const GoogleAuth = gapi.auth2.getAuthInstance();
+              if (GoogleAuth.isSignedIn.get()) {
+                loadClient(function () {
+                  $("#loading").hide();
+                  $("#contents").show();
+                });
+              } else {
+                $("#fetch").hide();
+                $("#connection").show();
+                $("#loading").hide();
+                $("#contents").show();
+              }
             });
-          } else {
-            $("#fetch").hide();
-            $("#connection").show();
-            $("#loading").hide();
-            $("#contents").show();
-          }
-        });
+        }
+      );
     }
-  );
+  }
 });
